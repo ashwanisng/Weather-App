db804561d670d5ef69d42bd8f241eca16489445e
ashwanisng <ashwanicena5@gmail.com>
2021-01-25 11:44:07 +0530

    added the scrrens

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/constant.dart';
+import 'package:weather_app/utilities/constant.dart';
 
 class LocationScreen extends StatefulWidget {
   @override
--- lib/utilities/constant.dart
+++ lib/utilities/constant.dart
@@ -1,12 +1,22 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 const kTempTextStyle = TextStyle(
   fontSize: 100.0,
+  fontFamily: 'Rubik-Regular',
 );
 const kConditionStyle = TextStyle(
   fontSize: 100.0,
+  fontFamily: 'Rubik-Regular',
 );
 
 const kMessageTextStyle = TextStyle(
   fontSize: 60.0,
+  fontFamily: 'Source Code Pro',
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Rubik-Regular',
 );
