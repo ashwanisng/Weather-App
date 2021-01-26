71200027f8d15581c3005fd201e5b559374d9409
ashwanisng <ashwanicena5@gmail.com>
2021-01-26 13:39:02 +0530

    netwroking page donr

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:weather_app/services/location.dart';
+import 'package:weather_app/services/networking.dart';
+
+const apiKey = 'b6edd5f70976317e420c7c7ae67614e5';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,41 +10,38 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
 
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
-  }
+    latitude = location.latitude;
+    longitude = location.longitude;
 
-  void getData() async {
-    http.Response response = await http.get(
-        'http://api.openweathermap.org/data/2.5/weather?q=Delhi,in&appid=b6edd5f70976317e420c7c7ae67614e5');
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      var temprature = jsonDecode(data)['main']['temp'];
-      print(temprature);
-      var condition = jsonDecode(data)['weather'][0]['id'];
-      print(condition);
-      var cityName = jsonDecode(data)['name'];
-      print(cityName);
-    } else {
-      print(response.statusCode);
-    }
+    var weatherData = networkHelper.getData();
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold();
   }
 }
+
+/**
+//  * var temprature = jsonDecode(data)['main']['temp'];
+ *  print(temprature);
+//  * var condition = jsonDecode(data)['weather'][0]['id'];
+  print(condition);
+  // var cityName = jsonDecode(data)['name'];
+  print(cityName);
+ */
